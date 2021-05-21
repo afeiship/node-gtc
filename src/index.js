62b1f81cb325ea2ffa0ade1213eb180423e17518
afeiship <1290657123@qq.com>
2021-05-21 11:45:08 +0800

    feat: rm fancy log

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 (function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
-  var nxLog = nx.log || require('@jswork/next-log');
   var DEFAULT_OPTIONS = { debug: false };
 
   nx.fetchWithDebug = function (inFetch) {
     return function (inUrl, inOptions) {
       var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
-      options.debug && nxLog(inUrl, options);
+      options.debug && console.log(inUrl, options);
       return inFetch(inUrl, options);
     };
   };
